54955532736c3b0fdc7f3090aa218ccdbfc8cece
wwwxylan <150700512+wwwxylan@users.noreply.github.com>
2026-09-09 21:18:49 -0500

    Update creds.py

--- creds.py
+++ creds.py
@@ -5,7 +5,8 @@ load_dotenv(find_dotenv())
 
 
 class Credentials:
-    API_ID: int = config("API_ID")
+    API_ID: int = config("API_ID", cast=int)
     API_HASH: str = config("API_HASH")
     BOT_TOKEN: str = config("BOT_TOKEN")
     USER_NAME: str = config("USER_NAME")
+    SESSION_STRING: str = config("SESSION_STRING")
